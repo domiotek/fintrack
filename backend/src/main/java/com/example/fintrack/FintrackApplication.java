c82ef9ee4f0581468b52997a6f4155a0aac27e94
Mateusz-Platek <m.platek1705@gmail.com>
2025-05-14 14:13:29 +0200

    Added separate data initializer

--- backend/src/main/java/com/example/fintrack/FintrackApplication.java
+++ backend/src/main/java/com/example/fintrack/FintrackApplication.java
@@ -1,27 +1,12 @@
 package com.example.fintrack;
 
-import com.example.fintrack.currency.Currency;
-import com.example.fintrack.currency.CurrencyRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FintrackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FintrackApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner init(
-			final CurrencyRepository currencyRepository
-	) {
-		return args -> {
-			Currency pln = new Currency();
-			pln.setName("PLN");
-			currencyRepository.save(pln);
-		};
 	}
 }
