46527b7b3227e1eb99252dd1be4928e7b96dd63f
Mateusz-Platek <m.platek1705@gmail.com>
2025-05-17 17:21:05 +0200

    Redis host name changed

--- backend/src/main/java/com/example/fintrack/config/RedisConfig.java
+++ backend/src/main/java/com/example/fintrack/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.fintrack.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -9,10 +10,12 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 public class RedisConfig {
 
+    @Value("${SPRING_CACHE_DB_HOST:localhost}")
+    private String host;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration =
-                new RedisStandaloneConfiguration("localhost", 6379);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, 6379);
         return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
