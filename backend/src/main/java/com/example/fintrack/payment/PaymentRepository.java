39df938ef8a5d727784959430fcf7ec0639ff2ac
Konrad <126018488+SimbaHokage@users.noreply.github.com>
2025-05-18 23:24:10 +0200

    fixed entities (#29)

--- backend/src/main/java/com/example/fintrack/payment/PaymentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.fintrack.payment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
