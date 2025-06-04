a3c06808a646cff7ee5ce327ad64474648059759
Damian <damianomiotek@outlook.com>
2025-06-04 23:19:28 +0200

    Add error codes for self-invite and already friends to ApiErrorCode enum and update error messages in BaseApiService

--- front/src/app/core/services/base-api.service.ts
+++ front/src/app/core/services/base-api.service.ts
@@ -13,6 +13,10 @@ export class BaseApiService {
       case 300:
       case 304:
         return 'Błąd przy próbie uwierzytelnienia. Pomocne może być usunięcie plików cookies i ponowienie operacji.';
+      case 601:
+        return 'Nie możesz zaprosić sam siebie do znajomych.';
+      case 602:
+        return 'Jesteście już znajomymi.';
       default:
         return 'Wystąpił nieznany błąd.';
     }
