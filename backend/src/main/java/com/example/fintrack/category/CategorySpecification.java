e2c2928c50d0b8ffc92f922c3a0e09e0efad0609
Mateusz Płatek <m.platek1705@gmail.com>
2025-06-04 21:04:18 +0200

    Changed limit to amount and added conversion between currencies when adding and getting limit (#70)

--- backend/src/main/java/com/example/fintrack/category/CategorySpecification.java
+++ backend/src/main/java/com/example/fintrack/category/CategorySpecification.java
@@ -1,12 +1,6 @@
 package com.example.fintrack.category;
 
-import com.example.fintrack.bill.Bill;
-import com.example.fintrack.limit.Limit;
-import jakarta.persistence.criteria.Join;
-import jakarta.persistence.criteria.JoinType;
 import org.springframework.data.jpa.domain.Specification;
-
-import java.time.ZonedDateTime;
 
 public class CategorySpecification {
 
@@ -16,35 +10,5 @@ public class CategorySpecification {
 
     public static Specification<Category> hasCategoryName(String categoryName) {
         return (root, query, builder) ->  builder.like(builder.lower(root.get("name")), "%" + categoryName.toLowerCase() + "%");
-    }
-
-    public static Specification<Category> hasCategoryBillsBetween(ZonedDateTime from, ZonedDateTime to) {
-        return (root, query, builder) -> {
-            Join<Category, Bill> billJoin = root.join("bills", JoinType.LEFT);
-            return builder.or(
-                    builder.between(billJoin.get("date"), from, to),
-                    builder.isNull(billJoin.get("id"))
-            );
-        };
-    }
-
-    public static Specification<Category> hasCategoryLimitsAfter(ZonedDateTime from) {
-        return (root, query, builder) -> {
-            Join<Category, Limit> limitJoin = root.join("limits", JoinType.LEFT);
-            return builder.or(
-                    builder.greaterThan(limitJoin.get("startDateTime"), from),
-                    builder.isNull(limitJoin.get("id"))
-            );
-        };
-    }
-
-    public static Specification<Category> hasCategoryLimitsBefore(ZonedDateTime to) {
-        return (root, query, builder) -> {
-            Join<Category, Limit> limitJoin = root.join("limits", JoinType.LEFT);
-            return builder.or(
-                    builder.lessThan(limitJoin.get("endDateTime"), to),
-                    builder.isNull(limitJoin.get("id"))
-            );
-        };
     }
 }
