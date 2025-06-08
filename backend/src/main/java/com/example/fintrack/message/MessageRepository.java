0fab2df889367641eada288ccdc6b4792f8ed218
Konrad <126018488+SimbaHokage@users.noreply.github.com>
2025-06-08 21:30:12 +0200

    getChatState fixed (#93)

--- backend/src/main/java/com/example/fintrack/message/MessageRepository.java
+++ backend/src/main/java/com/example/fintrack/message/MessageRepository.java
@@ -3,8 +3,13 @@ package com.example.fintrack.message;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     Page<Message> getMessagesByIdLessThanEqualAndChatId(long messageId, long chatId, Pageable pageable);
+
+    @Query("SELECT m from Message m where m.chat.id = :chatId order by m.id DESC")
+    Page<Message> getFirstMessagesByChatId(@Param("chatId")long chatId, Pageable pageable);
 }
