ed7b54fcdbd36081b529022260f6b4d511a3370e
Konrad <126018488+SimbaHokage@users.noreply.github.com>
2025-06-09 13:02:21 +0200

    HandshakeInterceptor (#94)

---
+++ backend/src/main/java/com/example/fintrack/security/service/CustomHandshakeHandler.java
@@ -0,0 +1,20 @@
+package com.example.fintrack.security.service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.security.Principal;
+import java.util.Map;
+
+@Configuration
+public class CustomHandshakeHandler extends DefaultHandshakeHandler {
+
+    @Override
+    protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+        return (Authentication) attributes.get("user");
+    }
+
+}
