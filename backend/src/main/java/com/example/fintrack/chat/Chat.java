fcbd242f3601aa39acc345ac64eb17cf853235b7
tnt120 <48412587+tnt120@users.noreply.github.com>
2025-06-12 00:29:20 +0200

    fix: chat optimalization fix (#144)

--- backend/src/main/java/com/example/fintrack/chat/Chat.java
+++ backend/src/main/java/com/example/fintrack/chat/Chat.java
@@ -21,7 +21,7 @@ public class Chat {
     @GeneratedValue
     private Long id;
 
-    @OneToOne(mappedBy = "chat")
+    @OneToOne(mappedBy = "chat", fetch = FetchType.LAZY)
     private Event event;
 
     @OneToMany(mappedBy = "chat")
